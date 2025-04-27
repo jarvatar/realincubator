9d6454a6cceddbd926e0c31303890d02eee0cfe0
jarvatar <jarvisteam@gmail.com>
2025-04-27 15:29:02 -0400

    added cloudflrare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     // Configure Tailwind integration explicitly
     configFile: './tailwind.config.js',
-  })]
+  })],
+
+  adapter: cloudflare()
 });
